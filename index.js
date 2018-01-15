6e474c49924f889f9710236ecf50085c87b15e4c
thongbbc <nguyenanhthong1995@gmail.com>
2018-01-15 10:31:38 +0700

    Update

--- index.js
+++ index.js
@@ -100,6 +100,19 @@ app.get('/removeAccount',function(req,res) {
 		 	})
 
 })
+app.post('/allCustomer',urlencodedParser,function(req,res) {
+  if (req.body.password == '123'){
+    customer.find({}, function(err, data) {
+        if (data.length!=0) {
+          res.send({status:'OK',data:data})
+        } else {
+          res.send({status:'ERROR'})
+        }
+    })
+    } else {
+      res.send({status:'ERROR'})
+    }
+})
 
 
 io.on('connection', function(socket) {
